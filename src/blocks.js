8ecf5e951b8fb99915d2ac8f8bc2852f013d19d8
Rogério Carrasqueira <rogerio.carrasqueira@gmail.com>
2017-07-18 12:13:27 -0300

    More tabs adjustments

--- src/blocks.js
+++ src/blocks.js
@@ -84,10 +84,10 @@ define(function() {
       category: opt.categoryLabel,
       attributes: {class:'gjs-fonts gjs-f-text'},
       content: {
-        type: 'text',
-        content: 'Insert your text here',
-        style: { padding: '10px' },
-        activeOnRender: 1
+       type: 'text',
+       content: 'Insert your text here',
+       style: { padding: '10px' },
+       activeOnRender: 1
       },
     });
     bm.add('text-sect', {
