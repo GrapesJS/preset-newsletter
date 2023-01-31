7705acc956561d6cecc47d350a718ca897c6954b
Artur Arseniev <artur.catch@hotmail.it>
2023-01-31 17:19:19 +0400

    Move toggleImagesCommand to TS

---
+++ src/toggleImagesCommand.ts
@@ -0,0 +1,37 @@
+import type grapesjs from 'grapesjs';
+import { PluginOptions } from './blocks';
+
+export default (editor: grapesjs.Editor,  opts: Required<PluginOptions>) => {
+  editor.Commands.add(opts.cmdTglImages, {
+    run(editor) {
+        const components = editor.getComponents();
+        this.toggleImages(components);
+    },
+
+    stop(editor) {
+        const components = editor.getComponents();
+        this.toggleImages(components, true);
+    },
+
+    toggleImages(components: any, on: boolean = false) {
+        const srcPlh = '##';
+
+        components.forEach((component: grapesjs.Component) => {
+            if (component.get('type') === 'image') {
+                const source = component.get('src');
+
+                if (on) {
+                    if (source === srcPlh) {
+                        component.set('src', component.get('src_bkp'));
+                    }
+                } else if (source !== srcPlh) {
+                    component.set('src_bkp', component.get('src'));
+                    component.set('src', srcPlh);
+                }
+            }
+
+            this.toggleImages(component.get('components'), on);
+        });
+    },
+});
+};
