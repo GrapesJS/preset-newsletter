2f3485e9397dafeb496c2b53544b0ef6d0f46992
Artur Arseniev <artur.catch@hotmail.it>
2023-01-31 17:23:15 +0400

    Move command to TS

--- src/commands.ts
+++ src/commands.ts
@@ -1,46 +1,51 @@
+import type grapesjs from 'grapesjs';
+import openImportCommand from './openImportCommand';
+import openExportCommand from './openExportCommand';
 import tglImagesCommand from './toggleImagesCommand';
+import { PluginOptions } from './blocks'; // TODO MOVE
 
-define(function() {
-  return (opt = {}) => {
-    let editor = opt.editor;
-    let cmdm = editor.Commands;
-    let importCommand = require('./openImportCommand');
-    let exportCommand = require('./openExportCommand');
-    cmdm.add(opt.cmdOpenImport, importCommand(opt));
-    cmdm.add(opt.cmdTglImages, tglImagesCommand(opt));
+export default (editor: grapesjs.Editor, opts: Required<PluginOptions>) => {
+    const cmdm = editor.Commands;
+
+    openImportCommand(editor, opts);
+    openExportCommand(editor, opts);
+    tglImagesCommand(editor, opts);
 
     // Overwrite export template after the editor is loaded
     // (default commands are loaded after plugins)
-    editor.on('load', () => {
-      cmdm.add('export-template', exportCommand(opt));
-    });
+    // editor.on('load', () => {
+    //   cmdm.add('export-template', exportCommand(opt));
+    // });
 
     cmdm.add('undo', {
       run(editor, sender) {
         sender.set('active', 0);
-        editor.UndoManager.undo(1);
+        editor.UndoManager.undo();
       }
     });
+
     cmdm.add('redo', {
       run(editor, sender) {
         sender.set('active', 0);
-        editor.UndoManager.redo(1);
+        editor.UndoManager.redo();
       }
     });
+
     cmdm.add('set-device-desktop', {
       run(editor) {
         editor.setDevice('Desktop');
       }
     });
+
     cmdm.add('set-device-tablet', {
       run(editor) {
         editor.setDevice('Tablet');
       }
     });
+
     cmdm.add('set-device-mobile', {
       run(editor) {
         editor.setDevice('Mobile portrait');
       }
     });
-  };
-})
+};
