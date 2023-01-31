f1a1b7962e252c1429e1789a9dcfb9545a0dac1d
Artur Arseniev <artur.catch@hotmail.it>
2023-01-31 17:39:52 +0400

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import type grapesjs from 'grapesjs';
 import juice from 'juice';
+import loadCommands from './commands';
+import loadBlocks from './blocks';
+import loadButtons from './buttons';
+import type grapesjs from 'grapesjs';
 
 export interface PluginOptions {
   /**
@@ -16,7 +19,7 @@ export interface PluginOptions {
 
   /**
    * Blocks category label.
-   * @default 'Basic'
+   * @default ''
    */
   categoryLabel?: string;
 
@@ -106,43 +109,42 @@ export interface PluginOptions {
 export type RequiredPluginOptions = Required<PluginOptions>;
 
 const plugin: grapesjs.Plugin<PluginOptions> = (editor, opts: Partial<PluginOptions> = {}) => {
-  let c = opts || {};
   let config = editor.getConfig();
-  let pfx = config.stylePrefix;
 
-  const defaults: RequiredPluginOptions = {
-    editor,
-    pfx: pfx || '',
+  const options: RequiredPluginOptions = {
+    blocks: [],
+    block: () => ({}),
+    juiceOpts: {},
     cmdOpenImport: 'gjs-open-import-template',
     cmdTglImages: 'gjs-toggle-images',
     cmdInlineHtml: 'gjs-get-inlined-html',
-    cmtTglImagesLabel: 'Toggle Images',
-    cmdBtnMoveLabel: 'Move',
-    cmdBtnUndoLabel: 'Undo',
-    cmdBtnRedoLabel: 'Redo',
-    cmdBtnDesktopLabel: 'Desktop',
-    cmdBtnTabletLabel: 'Tablet',
-    cmdBtnMobileLabel: 'Mobile',
+    // cmtTglImagesLabel: 'Toggle Images',
+    // cmdBtnMoveLabel: 'Move',
+    // cmdBtnUndoLabel: 'Undo',
+    // cmdBtnRedoLabel: 'Redo',
+    // cmdBtnDesktopLabel: 'Desktop',
+    // cmdBtnTabletLabel: 'Tablet',
+    // cmdBtnMobileLabel: 'Mobile',
     modalTitleImport: 'Import template',
     modalTitleExport: 'Export template',
     modalLabelImport: '',
     modalLabelExport: '',
     modalBtnImport: 'Import',
     codeViewerTheme: 'hopscotch',
-    openBlocksBtnTitle: c.openBlocksBtnTitle || '',
-    openLayersBtnTitle: c.openLayersBtnTitle || '',
-    openSmBtnTitle: c.openSmBtnTitle || '',
-    openTmBtnTitle: c.openTmBtnTitle || '',
-    expTplBtnTitle: c.expTplBtnTitle || 'View Code',
-    fullScrBtnTitle: c.fullScrBtnTitle || 'FullScreen',
-    swichtVwBtnTitle: c.swichtVwBtnTitle || 'View Components',
-    categoryLabel: c.categoryLabel || '',
+    // openBlocksBtnTitle: opts.openBlocksBtnTitle || '',
+    // openLayersBtnTitle: opts.openLayersBtnTitle || '',
+    // openSmBtnTitle: opts.openSmBtnTitle || '',
+    // openTmBtnTitle: opts.openTmBtnTitle || '',
+    // expTplBtnTitle: opts.expTplBtnTitle || 'View Code',
+    // fullScrBtnTitle: opts.fullScrBtnTitle || 'FullScreen',
+    // swichtVwBtnTitle: opts.swichtVwBtnTitle || 'View Components',
+    categoryLabel: '',
     importPlaceholder: '',
-    defaultTemplate: '', // Default template in case the canvas is empty
-    inlineCss: 1,
+    // defaultTemplate: '', // Default template in case the canvas is empty
+    inlineCss: true,
     cellStyle: {
-      padding: 0,
-      margin: 0,
+      padding: '0',
+      margin: '0',
       'vertical-align': 'top',
     },
     tableStyle: {
@@ -151,183 +153,173 @@ const plugin: grapesjs.Plugin<PluginOptions> = (editor, opts: Partial<PluginOpti
       padding: '5px 5px 5px 5px',
       width: '100%'
     },
-    sect100BlkLabel: '1 Section',
-    sect50BlkLabel: '1/2 Section',
-    sect30BlkLabel: '1/3 Section',
-    sect37BlkLabel: '3/7 Section',
-    buttonBlkLabel: 'Button',
-    dividerBlkLabel: 'Divider',
-    textBlkLabel: 'Text',
-    textSectionBlkLabel: 'Text Section',
-    imageBlkLabel: 'Image',
-    quoteBlkLabel: 'Quote',
-    linkBlkLabel: 'Link',
-    linkBlockBlkLabel: 'Link Block',
-    gridItemsBlkLabel: 'Grid Items',
-    listItemsBlkLabel: 'List Items',
-    assetsModalTitle: c.assetsModalTitle || 'Select image',
-    styleManagerSectors: [{
-        name: 'Dimension',
-        open: false,
-        buildProps: ['width', 'height', 'max-width', 'min-height', 'margin', 'padding'],
-        properties:[{
-          property: 'margin',
-          properties:[
-            { name: 'Top', property: 'margin-top'},
-            { name: 'Left', property: 'margin-left'},
-            { name: 'Right', property: 'margin-right'},
-            { name: 'Bottom', property: 'margin-bottom'}
-          ],
-        },{
-          property  : 'padding',
-          properties:[
-            { name: 'Top', property: 'padding-top'},
-            { name: 'Right', property: 'padding-right'},
-            { name: 'Bottom', property: 'padding-bottom'},
-            { name: 'Left', property: 'padding-left'}
-          ],
-        }],
-      },{
-        name: 'Typography',
-        open: false,
-        buildProps: ['font-family', 'font-size', 'font-weight', 'letter-spacing', 'color', 'line-height', 'text-align', 'text-decoration', 'font-style', 'vertical-align', 'text-shadow'],
-        properties:[
-          { name: 'Font', property: 'font-family'},
-          { name: 'Weight', property: 'font-weight'},
-          { name: 'Font color', property: 'color'},
-          {
-            property: 'text-align',
-            type: 'radio',
-            defaults: 'left',
-            list: [
-              { value: 'left', name: 'Left', className: 'fa fa-align-left'},
-              { value: 'center', name: 'Center', className: 'fa fa-align-center' },
-              { value: 'right', name: 'Right', className: 'fa fa-align-right'},
-              { value: 'justify', name: 'Justify', className: 'fa fa-align-justify'}
-            ],
-          },{
-            property: 'text-decoration',
-            type: 'radio',
-            defaults: 'none',
-            list: [
-              { value: 'none', name: 'None', className: 'fa fa-times'},
-              { value: 'underline', name: 'underline', className: 'fa fa-underline' },
-              { value: 'line-through', name: 'Line-through', className: 'fa fa-strikethrough'}
-            ],
-          },{
-            property: 'font-style',
-            type: 'radio',
-            defaults: 'normal',
-            list: [
-              { value: 'normal', name: 'Normal', className: 'fa fa-font'},
-              { value: 'italic', name: 'Italic', className: 'fa fa-italic'}
-            ],
-          },{
-            property: 'vertical-align',
-            type: 'select',
-            defaults: 'baseline',
-            list: [
-              { value: 'baseline'},
-              { value: 'top'},
-              { value: 'middle'},
-              { value: 'bottom'}
-            ],
-          },{
-            property: 'text-shadow',
-            properties: [
-              { name: 'X position', property: 'text-shadow-h'},
-              { name: 'Y position', property: 'text-shadow-v'},
-              { name: 'Blur', property: 'text-shadow-blur'},
-              { name: 'Color', property: 'text-shadow-color'}
-            ],
-        }],
-      },{
-        name: 'Decorations',
-        open: false,
-        buildProps: ['background-color', 'border-collapse', 'border-radius', 'border', 'background'],
-        properties: [{
-          property: 'background-color',
-          name: 'Background',
-        },{
-          property: 'border-radius',
-          properties  : [
-            { name: 'Top', property: 'border-top-left-radius'},
-            { name: 'Right', property: 'border-top-right-radius'},
-            { name: 'Bottom', property: 'border-bottom-left-radius'},
-            { name: 'Left', property: 'border-bottom-right-radius'}
-          ],
-        },{
-          property: 'border-collapse',
-          type: 'radio',
-          defaults: 'separate',
-          list: [
-            { value: 'separate', name: 'No'},
-            { value: 'collapse', name: 'Yes'}
-          ],
-        },
-        /*
-        { // Too much low support
-          property: 'box-shadow',
-          properties: [
-            { name: 'X position', property: 'box-shadow-h'},
-            { name: 'Y position', property: 'box-shadow-v'},
-            { name: 'Blur', property: 'box-shadow-blur'},
-            { name: 'Spread', property: 'box-shadow-spread'},
-            { name: 'Color', property: 'box-shadow-color'},
-            { name: 'Shadow type', property: 'box-shadow-type'}
-          ],
-        },*/{
-          property: 'border',
-          properties: [
-            { name: 'Width', property: 'border-width', defaults: '0'},
-            { name: 'Style', property: 'border-style'},
-            { name: 'Color', property: 'border-color'},
-          ],
-        },{
-          property: 'background',
-          properties: [
-            { name: 'Image', property: 'background-image'},
-            { name: 'Repeat', property:   'background-repeat'},
-            { name: 'Position', property: 'background-position'},
-            { name: 'Attachment', property: 'background-attachment'},
-            { name: 'Size', property: 'background-size'}
-          ],
-        }],
-      }],
-
+    // sect100BlkLabel: '1 Section',
+    // sect50BlkLabel: '1/2 Section',
+    // sect30BlkLabel: '1/3 Section',
+    // sect37BlkLabel: '3/7 Section',
+    // buttonBlkLabel: 'Button',
+    // dividerBlkLabel: 'Divider',
+    // textBlkLabel: 'Text',
+    // textSectionBlkLabel: 'Text Section',
+    // imageBlkLabel: 'Image',
+    // quoteBlkLabel: 'Quote',
+    // linkBlkLabel: 'Link',
+    // linkBlockBlkLabel: 'Link Block',
+    // gridItemsBlkLabel: 'Grid Items',
+    // listItemsBlkLabel: 'List Items',
+    // assetsModalTitle: opts.assetsModalTitle || 'Select image',
+    // styleManagerSectors: [],
     ...opts,
   };
 
+  const styleManagerSectors = [{
+    name: 'Dimension',
+    open: false,
+    buildProps: ['width', 'height', 'max-width', 'min-height', 'margin', 'padding'],
+    properties:[{
+      property: 'margin',
+      properties:[
+        { name: 'Top', property: 'margin-top'},
+        { name: 'Left', property: 'margin-left'},
+        { name: 'Right', property: 'margin-right'},
+        { name: 'Bottom', property: 'margin-bottom'}
+      ],
+    },{
+      property  : 'padding',
+      properties:[
+        { name: 'Top', property: 'padding-top'},
+        { name: 'Right', property: 'padding-right'},
+        { name: 'Bottom', property: 'padding-bottom'},
+        { name: 'Left', property: 'padding-left'}
+      ],
+    }],
+  },{
+    name: 'Typography',
+    open: false,
+    buildProps: ['font-family', 'font-size', 'font-weight', 'letter-spacing', 'color', 'line-height', 'text-align', 'text-decoration', 'font-style', 'vertical-align', 'text-shadow'],
+    properties:[
+      { name: 'Font', property: 'font-family'},
+      { name: 'Weight', property: 'font-weight'},
+      { name: 'Font color', property: 'color'},
+      {
+        property: 'text-align',
+        type: 'radio',
+        defaults: 'left',
+        list: [
+          { value: 'left', name: 'Left', className: 'fa fa-align-left'},
+          { value: 'center', name: 'Center', className: 'fa fa-align-center' },
+          { value: 'right', name: 'Right', className: 'fa fa-align-right'},
+          { value: 'justify', name: 'Justify', className: 'fa fa-align-justify'}
+        ],
+      },{
+        property: 'text-decoration',
+        type: 'radio',
+        defaults: 'none',
+        list: [
+          { value: 'none', name: 'None', className: 'fa fa-times'},
+          { value: 'underline', name: 'underline', className: 'fa fa-underline' },
+          { value: 'line-through', name: 'Line-through', className: 'fa fa-strikethrough'}
+        ],
+      },{
+        property: 'font-style',
+        type: 'radio',
+        defaults: 'normal',
+        list: [
+          { value: 'normal', name: 'Normal', className: 'fa fa-font'},
+          { value: 'italic', name: 'Italic', className: 'fa fa-italic'}
+        ],
+      },{
+        property: 'vertical-align',
+        type: 'select',
+        defaults: 'baseline',
+        list: [
+          { value: 'baseline'},
+          { value: 'top'},
+          { value: 'middle'},
+          { value: 'bottom'}
+        ],
+      },{
+        property: 'text-shadow',
+        properties: [
+          { name: 'X position', property: 'text-shadow-h'},
+          { name: 'Y position', property: 'text-shadow-v'},
+          { name: 'Blur', property: 'text-shadow-blur'},
+          { name: 'Color', property: 'text-shadow-color'}
+        ],
+    }],
+  },{
+    name: 'Decorations',
+    open: false,
+    buildProps: ['background-color', 'border-collapse', 'border-radius', 'border', 'background'],
+    properties: [{
+      property: 'background-color',
+      name: 'Background',
+    },{
+      property: 'border-radius',
+      properties  : [
+        { name: 'Top', property: 'border-top-left-radius'},
+        { name: 'Right', property: 'border-top-right-radius'},
+        { name: 'Bottom', property: 'border-bottom-left-radius'},
+        { name: 'Left', property: 'border-bottom-right-radius'}
+      ],
+    },{
+      property: 'border-collapse',
+      type: 'radio',
+      defaults: 'separate',
+      list: [
+        { value: 'separate', name: 'No'},
+        { value: 'collapse', name: 'Yes'}
+      ],
+    },
+    /*
+    { // Too much low support
+      property: 'box-shadow',
+      properties: [
+        { name: 'X position', property: 'box-shadow-h'},
+        { name: 'Y position', property: 'box-shadow-v'},
+        { name: 'Blur', property: 'box-shadow-blur'},
+        { name: 'Spread', property: 'box-shadow-spread'},
+        { name: 'Color', property: 'box-shadow-color'},
+        { name: 'Shadow type', property: 'box-shadow-type'}
+      ],
+    },*/{
+      property: 'border',
+      properties: [
+        { name: 'Width', property: 'border-width', defaults: '0'},
+        { name: 'Style', property: 'border-style'},
+        { name: 'Color', property: 'border-color'},
+      ],
+    },{
+      property: 'background',
+      properties: [
+        { name: 'Image', property: 'background-image'},
+        { name: 'Repeat', property:   'background-repeat'},
+        { name: 'Position', property: 'background-position'},
+        { name: 'Attachment', property: 'background-attachment'},
+        { name: 'Size', property: 'background-size'}
+      ],
+    }],
+  }];
+
   // Change some config
   config.devicePreviewMode = true;
+  const { Panels } = editor;
 
-  // Add commands
-  let importCommands = require('./commands');
-  importCommands(c);
-
-  // Add blocks
-  let importBlocks = require('./blocks');
-  importBlocks(c);
-
-  // Add buttons
-  let importButtons = require('./buttons');
-  importButtons(c);
-
-  // Load style manager
-  let importStyle = require('./style');
-  importStyle(c);
+  loadCommands(editor, options);
+  loadBlocks(editor, options);
+  loadButtons(editor, options);
 
   // On component change show the Style Manager
-  editor.on('change:selectedComponent', function() {
-    var openLayersBtn = editor.Panels.getButton('views', 'open-layers');
+  editor.on('component:toggled', function() {
+    const openLayersBtn = Panels.getButton('views', 'open-layers');
 
-    // Don't switch when the Layer Manager is on or
-    // there is no selected component
-    if((!openLayersBtn || !openLayersBtn.get('active')) &&
-      editor.editor.get('selectedComponent')){
-      var openSmBtn = editor.Panels.getButton('views', 'open-sm');
-      openSmBtn.set('attributes',{ title:defaults.openSmBtnTitle });
-      openSmBtn && openSmBtn.set('active', 1);
+    // Don't switch when the Layer Manager is on or there is no selected components
+    if((!openLayersBtn || !openLayersBtn.get('active')) && editor.getSelected()){
+      const openSmBtn = Panels.getButton('views', 'open-sm');
+      if (openSmBtn) {
+        openSmBtn.set('attributes',{ title: options.openSmBtnTitle });
+      openSmBtn && openSmBtn.set('active', true);
+      }
     }
   });
 
@@ -339,7 +331,6 @@ const plugin: grapesjs.Plugin<PluginOptions> = (editor, opts: Partial<PluginOpti
 
   // Do stuff on load
   editor.onReady(function() {
-    const { Panels } = editor;
     Panels.getButton('options', 'export-template')?.set('attributes', {
       title: defaults.expTplBtnTitle
     });
