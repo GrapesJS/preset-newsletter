074c364a66b65c2d81ac2108491b5052789731d8
Artur Arseniev <artur.catch@hotmail.it>
2023-02-01 14:30:48 +0400

    Update panels

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const cmdDeviceDesktop = 'set-device-desktop';
+export const cmdDeviceTablet = 'set-device-tablet';
+export const cmdDeviceMobile = 'set-device-mobile';
+export const cmdClear = 'canvas-clear';
